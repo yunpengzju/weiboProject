447a0396ec679e57e6c0ed259d30c4efbc730618
turkey <mathcyp2008@hotmail.com>
2013-09-05 23:17:53 +0800

    代码微调，功能不变

--- txweibo/weibo.py
+++ txweibo/weibo.py
@@ -2,7 +2,6 @@
 #! /usr/bin/env python
 
 import time
-#sys.path.insert(0, 'tweibo.zip')
 from tweibo import *
 import cPickle
 
@@ -10,45 +9,39 @@ import cPickle
 APP_KEY = "801058005"
 APP_SECRET = "31cc09205420a004f3575467387145a7"
 CALLBACK_URL = "https://test.open.t.qq.com"
-# 请先按照 https://github.com/upbit/tweibo-pysdk/wiki/OAuth2Handler 的鉴权说明填写 ACCESS_TOKEN 和 OPENID
 ACCESS_TOKEN = "e064789cdc8c80df50e0519d02882d70"
 OPENID = "BCB210D551A94AF3D18D3C88E51668DB"
 IMG_EXAMPLE = "example.png"
 
-# 返回text是unicode，设置默认编码为utf8
+# 默认字符集utf-8
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+# 用户验证
 def access_token_test():
-    """ 访问get_access_token_url()的URL并授权后，会跳转callback页面，其中包含如下参数：
-        #access_token=00000000000ACCESSTOKEN0000000000&expires_in=8035200&openid=0000000000000OPENID0000000000000&openkey=0000000000000OPENKEY000000000000&refresh_token=0000000000REFRESHTOKEN00000000&state=
-    保存下其中的 access_token, openid 并调用
-        oauth.set_access_token(access_token)
-        oauth.set_openid(openid)
-    即可完成 OAuth2Handler() 的初始化。可以记录 access_token 等信息
-    """
     oauth = OAuth2Handler()
     oauth.set_app_key_secret(APP_KEY, APP_SECRET, CALLBACK_URL)
     print oauth.get_access_token_url()
 
+# 将系统时间戳转换成标准的日期格式
 def timestamp_datetime(value):
     format = '%Y-%m-%d %H:%M:%S'
     # value为传入的值为时间戳(整形)，如：1332888820
     value = time.localtime(value)
-    ## 经过localtime转换后变成
-    ## time.struct_time(tm_year=2012, tm_mon=3, tm_mday=28, tm_hour=6, tm_min=53, tm_sec=40, tm_wday=2, tm_yday=88, tm_isdst=0)
-    # 最后再经过strftime函数转换为正常日期格式。
+    # 经过strftime函数转换为正常日期格式。
     dt = time.strftime(format, value)
     return dt
 
+# 通过api获取微博
 def tweibo_test():
 	oauth = OAuth2Handler()
 	oauth.set_app_key_secret(APP_KEY, APP_SECRET, CALLBACK_URL)
 	oauth.set_access_token(ACCESS_TOKEN)
 	oauth.set_openid(OPENID)
 	api = API(oauth)
-	lo = 120
+	
+	lo = 120  # 测试时只固定尝试了一个经纬度值的微博情况。实际使用时可使用嵌套的循环遍历一批经纬度
 	la = 30
 	search = []	
 	try:
@@ -69,13 +62,11 @@ def tweibo_test():
 			for idx, tweet in enumerate(page.data.info):
 				print "[%d], [longitude:%s, latitude:%s] [%s],(%s),  %s" % (idx+1,tweet.longitude,tweet.latitude,tweet.location,timestamp_datetime(tweet.timestamp), tweet.text)
 		f = open("data/mydata","wb")
-		cPickle.dump(search,f)
+		cPickle.dump(search,f)      # 方便实验，将抓取到的json格式信息存到文件中保存；未来实际操作应存入数据库中
 		f.close()
 	except:
 		pass
 	
-	
-
 if __name__ == '__main__':
     #access_token_test()
     tweibo_test()
